07862f6b90c35d97d071a4a510d8a6e12c42bd5d
Bruno Camba <brunocamba@gmail.com>
2022-04-30 23:43:06 -0300

    Implemented find method for order repository

--- src/infrastructure/order/repository/sequilize/order.repository.spec.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.spec.ts
@@ -81,4 +81,38 @@ describe("Order repository test", () => {
       ],
     });
   });
+
+  it("should find an order", async () => {
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer("123", "Customer 1");
+    const address = new Address("Street 1", 1, "Zipcode 1", "City 1");
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
+    const productRepository = new ProductRepository();
+    const product = new Product("123", "Product 1", 10);
+    await productRepository.create(product);
+
+    const ordemItem = new OrderItem(
+        "1",
+        product.name,
+        product.price,
+        product.id,
+        2
+    );
+
+    const order = new Order("123", "123", [ordemItem]);
+
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order);
+
+    const orderModel = await OrderModel.findOne({
+      where: { id: "123" },
+      include: ["items"],
+    });
+
+    const orderFromDb = await orderRepository.find("123");
+
+    expect(orderFromDb).toStrictEqual(order)
+  });
 });
--- src/infrastructure/order/repository/sequilize/order.repository.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -1,8 +1,10 @@
 import Order from "../../../../domain/checkout/entity/order";
 import OrderItemModel from "./order-item.model";
 import OrderModel from "./order.model";
+import OrderItem from "../../../../domain/checkout/entity/order_item";
+import OrderRepositoryInterface from "../../../../domain/checkout/repository/order-repository.interface";
 
-export default class OrderRepository {
+export default class OrderRepository implements OrderRepositoryInterface{
   async create(entity: Order): Promise<void> {
     await OrderModel.create(
       {
@@ -21,5 +23,35 @@ export default class OrderRepository {
         include: [{ model: OrderItemModel }],
       }
     );
+  }
+
+  async find(id: string): Promise<Order> {
+    let orderModel;
+    try {
+        orderModel = await OrderModel.findOne({
+            where: { id },
+            include: ["items"],
+            rejectOnEmpty: true
+        });
+    } catch (error) {
+        throw new Error("Order not found");
+    }
+
+    return new Order(orderModel.id,
+        orderModel.customer_id,
+        orderModel.items.map(orderItemModel => new OrderItem(
+            orderItemModel.id,
+            orderItemModel.name,
+            orderItemModel.price,
+            orderItemModel.product_id,
+            orderItemModel.quantity)));
+  }
+
+  async findAll(): Promise<Order[]> {
+    return Promise.resolve([]);
+  }
+
+  async update(entity: Order): Promise<void> {
+    return Promise.resolve(undefined);
   }
 }
